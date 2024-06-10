0986a6e96d720af3ef853363ef0440fa2e32a3f0
yota sato <jingangsihaiuei@gmail.com>
2024-06-10 21:21:47 +0900

    プロミスの関数を定義したファイルを追加

---
+++ 03.asynchronous/promise_function.js
@@ -0,0 +1,47 @@
+import sqlite3 from "sqlite3";
+
+export function openDatabase() {
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database(":test2:", (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(db);
+      }
+    });
+  });
+}
+export function runQuery(db, query, params = []) {
+  return new Promise((resolve, reject) => {
+    db.run(query, params, function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(this);
+      }
+    });
+  });
+}
+
+export function getQuery(db, query, params = []) {
+  return new Promise((resolve, reject) => {
+    db.get(query, params, (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row);
+      }
+    });
+  });
+}
+export function closeDatabase(db) {
+  return new Promise((resolve, reject) => {
+    db.close((err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
