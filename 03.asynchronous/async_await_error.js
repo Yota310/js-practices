7c414579a2e4d9964c531ff80f50bc8e1c94b2d1
yota sato <jingangsihaiuei@gmail.com>
2024-06-10 21:25:36 +0900

    asyncのエラーありファイルを追加

---
+++ 03.asynchronous/async_await_error.js
@@ -0,0 +1,60 @@
+import {
+  openDatabase,
+  runQuery,
+  getQuery,
+  closeDatabase,
+} from "./async_await_function.js";
+
+async function main() {
+  let db;
+  try {
+    db = await openDatabase();
+    console.log("Connected to the in-memory SQlite database.");
+  } catch (err) {
+    console.error(`Error opening database: ${err.message}`);
+    return;
+  }
+  try {
+    await runQuery(
+      db,
+      `CREATE TABLE books (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          title TEXT UNIQUE NOT NULL
+      )`,
+    );
+    console.log("Created books table.");
+  } catch (err) {
+    console.error(`Error creating table: ${err.message}`);
+  }
+  let lastID;
+  try {
+    const result = await runQuery(db, `INSERT INTO books (name) VALUES (?)`, [
+      "桃太郎",
+    ]);
+    lastID = result.lastID;
+    console.log(`Inserted data id:${lastID}`);
+  } catch (err) {
+    console.error(`Error inserting books: ${err.message}`);
+    lastID = null;
+  }
+  try {
+    const row = await getQuery(db, `SELECT * FROM user WHERE id = ?`, [lastID]);
+    await console.log(`id: ${row.id}, title: ${row.title}`);
+  } catch (err) {
+    console.error(`Error selecting books: ${err.message}`);
+  }
+  try {
+    await runQuery(db, `DROP TABLE books`);
+    await console.log(`Droped TABLE books`);
+  } catch (err) {
+    console.error(`Error deleting books: ${err.message}`);
+  }
+
+  try {
+    await closeDatabase(db);
+    await console.log("Closed the database connection.");
+  } catch (err) {
+    console.error(`Error closing database: ${err.message}`);
+  }
+}
+main();
