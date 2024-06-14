9c4d0dd94c8fc107204508ef3ac003f5160958d7
yota sato <jingangsihaiuei@gmail.com>
2024-06-14 16:29:55 +0900

    データベース接続後にテーブルが作成されるように修正

--- 03.asynchronous/callback_no_error.js
+++ 03.asynchronous/callback_no_error.js
@@ -4,33 +4,38 @@ console.log("エラーなし");
 // データベースに接続
 const db = new sqlite3.Database(":memory:", () => {
   console.log("Connected to the in-memory SQlite database.");
-});
-// テーブルの作成
-db.run(
-  `CREATE TABLE books (
+
+  // テーブルの作成
+  db.run(
+    `CREATE TABLE books (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT UNIQUE NOT NULL 
 )`,
-  () => {
-    console.log("Created books table.");
-    // データの挿入
-    db.run(`INSERT INTO books (title) VALUES (?)`, ["桃太郎"], function () {
-      console.log(`Inserted data id:${this.lastID}`);
-      // データの読み込み
-      db.get(`SELECT * FROM books WHERE id = ?`, [this.lastID], (err, row) => {
-        if (err) {
-          return console.error(err.message);
-        }
-        console.log(row);
-        // テーブルの削除
-        db.run(`DROP TABLE books`, function () {
-          console.log(`DROP TABLE books`);
-          // データベースの切断
-          db.close(() => {
-            console.log("Closed the database connection.");
-          });
-        });
+    () => {
+      console.log("Created books table.");
+      // データの挿入
+      db.run(`INSERT INTO books (title) VALUES (?)`, ["桃太郎"], function () {
+        console.log(`Inserted data id:${this.lastID}`);
+        // データの読み込み
+        db.get(
+          `SELECT * FROM books WHERE id = ?`,
+          [this.lastID],
+          (err, row) => {
+            if (err) {
+              return console.error(err.message);
+            }
+            console.log(row);
+            // テーブルの削除
+            db.run(`DROP TABLE books`, function () {
+              console.log(`DROP TABLE books`);
+              // データベースの切断
+              db.close(() => {
+                console.log("Closed the database connection.");
+              });
+            });
+          },
+        );
       });
-    });
-  },
-);
+    },
+  );
+});
