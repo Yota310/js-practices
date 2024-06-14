f77edb79da021fdab2cf01c06da5613228fd8d20
yota sato <jingangsihaiuei@gmail.com>
2024-06-14 18:16:17 +0900

    不要なエラーハンドリングの削除

--- 03.asynchronous/callback_error.js
+++ 03.asynchronous/callback_error.js
@@ -2,10 +2,7 @@ import sqlite3 from "sqlite3";
 
 console.log("エラーあり");
 // データベースに接続
-let db = new sqlite3.Database(":memory:", (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
+let db = new sqlite3.Database(":memory:", () => {
   console.log("Connected to the in-memory SQlite database.");
 });
 // テーブルの作成
@@ -14,10 +11,7 @@ db.run(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT UNIQUE NOT NULL 
 )`,
-  (err) => {
-    if (err) {
-      console.error(err.message);
-    }
+  () => {
     console.log("Created books table.");
     // データの挿入
     db.run(`INSERT INTO books (title) VALUES (?)`, [], function (err) {
@@ -34,19 +28,11 @@ db.run(
           console.log(row);
         }
         // データの削除
-        db.run(`DROP TABLE books`, function (err) {
-          if (err) {
-            console.error(err.message);
-          } else {
+        db.run(`DROP TABLE books`, function () {
             console.log(`DROP TABLE books`);
-          }
           // データベースの切断
-          db.close((err) => {
-            if (err) {
-              console.error(err.message);
-            } else {
+          db.close(() => {
               console.log("Closed the database connection.");
-            }
           });
         });
       });
