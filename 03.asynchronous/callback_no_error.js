14dec01893c013da5feed5c31fddc77ece76e5f5
yota sato <jingangsihaiuei@gmail.com>
2024-06-14 15:53:34 +0900

    エラーハンドリングの削除

--- 03.asynchronous/callback_no_error.js
+++ 03.asynchronous/callback_no_error.js
@@ -2,10 +2,7 @@ import sqlite3 from "sqlite3";
 
 console.log("エラーなし");
 // データベースに接続
-const db = new sqlite3.Database(":memory:", (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
+const db = new sqlite3.Database(":memory:", () => {
   console.log("Connected to the in-memory SQlite database.");
 });
 // テーブルの作成
@@ -14,16 +11,10 @@ db.run(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT UNIQUE NOT NULL 
 )`,
-  (err) => {
-    if (err) {
-      return console.error(err.message);
-    }
+  () => {
     console.log("Created books table.");
     // データの挿入
-    db.run(`INSERT INTO books (title) VALUES (?)`, ["桃太郎"], function (err) {
-      if (err) {
-        return console.error(err.message);
-      }
+    db.run(`INSERT INTO books (title) VALUES (?)`, ["桃太郎"], function () {
       console.log(`Inserted data id:${this.lastID}`);
       // データの読み込み
       db.get(`SELECT * FROM books WHERE id = ?`, [this.lastID], (err, row) => {
@@ -32,16 +23,10 @@ db.run(
         }
         console.log(row);
         // テーブルの削除
-        db.run(`DROP TABLE books`, function (err) {
-          if (err) {
-            return console.error(err.message);
-          }
+        db.run(`DROP TABLE books`, function () {
           console.log(`DROP TABLE books`);
           // データベースの切断
-          db.close((err) => {
-            if (err) {
-              return console.error(err.message);
-            }
+          db.close(() => {
             console.log("Closed the database connection.");
           });
         });
