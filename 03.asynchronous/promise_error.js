7e2e2672b07326c72d29c3aca9e7c28bead61b18
yota sato <jingangsihaiuei@gmail.com>
2024-06-17 19:43:50 +0900

    Promiseのエラーさせる方でもthenがネストしている部分を修正

--- 03.asynchronous/promise_error.js
+++ 03.asynchronous/promise_error.js
@@ -4,62 +4,41 @@ import {
   getQuery,
   closeDatabase,
 } from "./promise_function.js";
+
 openDatabase()
   .then((db) => {
-    console.log("Connected to the in-memory SQlite database.");
     return runQuery(
       db,
-      `CREATE TABLE books (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          title TEXT UNIQUE NOT NULL
-      )`,
-    ).then(() => {
-      return db;
-    });
+      `CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT UNIQUE NOT NULL)`,
+    );
   })
-  .then((db) => {
+  .then((response) => {
     console.log("Created books table.");
-    return runQuery(db, `INSERT INTO books (name) VALUES (?)`, ["桃太郎"])
-      .then(function (result) {
-        console.log(`Inserted data id:${result.lastID}`);
-        return { db, lastID: result.lastID };
-      })
-      .catch((err) => {
-        console.error(err.message);
-        return { db, lastID: null };
-      });
+    return response;
   })
-
-  .then((context) => {
-    const { db, lastID } = context;
-    return getQuery(db, `SELECT * FROM user WHERE id = ?`, [lastID])
-      .then((row) => {
-        console.log(row);
-        return { db, lastID };
-      })
-      .catch((err) => {
-        console.error(err.message);
-        return { db, lastID };
-      });
+  .then((response) => {
+    const db = response.db;
+    return runQuery(db, `INSERT INTO books (name) VALUES (?)`, ["桃太郎"]);
   })
-  .then((context) => {
-    const { db } = context;
-    return runQuery(db, `DROP TABLE books`)
-      .then(function () {
-        console.log(`DROP TABLE books`);
-        return db;
-      })
-      .catch((err) => {
-        console.error(err.message);
-        return db;
-      });
+  .then((response) => {
+    const db = response.db;
+    const lastID = response.result.lastID;
+    console.log(response);
+    return getQuery(db, `SELECT * FROM user WHERE id = ?`, lastID);
   })
-
-  .then((db) => {
-    return closeDatabase(db).then(() => {
-      console.log("Closed the database connection.");
-    });
+  .then((response) => {
+    console.log("all books:", response.row);
+    return response;
   })
-  .catch((err) => {
-    console.error(err.message);
+  .then((response) => {
+    const db = response.db;
+    return runQuery(db, `DROP TABLE books`);
+  })
+  .then(function (response) {
+    const db = response.db;
+    console.log(`DROP TABLE books`);
+    return closeDatabase(db);
+  })
+  .then(() => {
+    console.log("Closed the database connection.");
   });
