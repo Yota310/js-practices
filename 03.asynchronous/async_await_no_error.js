bab5f243c7cff9662772a8765abb3ef039f5ab99
yota sato <jingangsihaiuei@gmail.com>
2024-06-17 19:31:50 +0900

    top-level awaitを使用するための修正

--- 03.asynchronous/async_await_no_error.js
+++ 03.asynchronous/async_await_no_error.js
@@ -4,60 +4,54 @@ import {
   getQuery,
   closeDatabase,
 } from "./async_await_function.js";
-
-async function main() {
-  let db;
-  try {
-    db = await openDatabase();
-    console.log("Connected to the in-memory SQlite database.");
-  } catch (err) {
-    console.error(`Error opening database: ${err.message}`);
-    return;
-  }
-  try {
-    await runQuery(
-      db,
-      `CREATE TABLE books (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          title TEXT UNIQUE NOT NULL
+let db;
+let response;
+try {
+  db = await openDatabase();
+  console.log("Connected to the in-memory SQlite database.");
+} catch (err) {
+  console.error(`Error opening database: ${err.message}`);
+}
+try {
+  response = await runQuery(
+    db,
+    `CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT UNIQUE NOT NULL
       )`,
-    );
-    console.log("Created books table.");
-  } catch (err) {
-    console.error(`Error creating table: ${err.message}`);
-  }
-  let lastID;
+  );
+  console.log("Created books table.");
+} catch (err) {
+  console.error(`Error creating table: ${err.message}`);
+}
+let lastID;
+try {
+  const db = response.db;
+  response = await runQuery(db, `INSERT INTO books (title) VALUES (?)`, [
+    "桃太郎",
+  ]);
+  lastID = response.result.lastID;
+  console.log(`Inserted data id:${lastID}`);
+} catch (err) {
+  console.error(`Error inserting books: ${err.message}`);
+}
+if (lastID !== null){
   try {
-    const result = await runQuery(db, `INSERT INTO books (title) VALUES (?)`, [
-      "桃太郎",
+    response = await getQuery(db, `SELECT * FROM books WHERE id = ?`, [
+      response.result.lastID,
     ]);
-    lastID = result.lastID;
-    console.log(`Inserted data id:${lastID}`);
+    console.log(response.row);
   } catch (err) {
-    console.error(`Error inserting books: ${err.message}`);
-    lastID = null;
-  }
-  if (lastID !== null) {
-    try {
-      const row = await getQuery(db, `SELECT * FROM books WHERE id = ?`, [
-        lastID,
-      ]);
-      await console.log(`id: ${row.id}, title: ${row.title}`);
-    } catch (err) {
-      console.error(`Error selecting books: ${err.message}`);
-    }
-    try {
-      await runQuery(db, `DROP TABLE books`);
-      await console.log(`Droped TABLE books`);
-    } catch (err) {
-      console.error(`Error deleting books: ${err.message}`);
-    }
+    console.error(`Error selecting books: ${err.message}`);
   }
   try {
-    await closeDatabase(db);
-    await console.log("Closed the database connection.");
+    await runQuery(response.db, `DROP TABLE books`);
+    console.log(`Dropped TABLE books`);
   } catch (err) {
-    console.error(`Error closing database: ${err.message}`);
+    console.error(`Error deleting books: ${err.message}`);
   }
 }
-main();
+try {
+  await closeDatabase(response.db);
+  console.log("Closed the database connection.");
+} catch (err) {
+  console.error(`Error closing database: ${err.message}`);
+}
