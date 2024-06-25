32aa4b9fd3ca3ccba904418fc46c3ec071618dc0
yota sato <jingangsihaiuei@gmail.com>
2024-06-25 15:33:39 +0900

    削除成功のメッセージを変更

--- 03.asynchronous/callback_no_error.js
+++ 03.asynchronous/callback_no_error.js
@@ -15,7 +15,7 @@ const db = new sqlite3.Database(":memory:", () => {
           (err, row) => {
           console.log(row);
           db.run("DROP TABLE books", () => {
-            console.log("DROP TABLE books");
+            console.log("Droped table books");
             db.close(() => {
               console.log("Closed the database connection.");
             });
