9b0200ffbe6ef7c82948a32f993b430311808d41
yota sato <jingangsihaiuei@gmail.com>
2024-06-25 15:25:13 +0900

    データベースに接続する前にテーブルを作らないように修正

--- 03.asynchronous/callback_error.js
+++ 03.asynchronous/callback_error.js
@@ -3,30 +3,30 @@ import sqlite3 from "sqlite3";
 console.log("エラーあり");
 const db = new sqlite3.Database(":memory:", () => {
   console.log("Connected to the in-memory SQlite database.");
-});
-db.run(
-  `CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)`,
-  () => {
-    console.log("Created books table.");
-    db.run(`INSERT INTO books (title) VALUES (?)`, function (err) {
-      if (err) {
-        console.error(`Error inserting books: ${err.message}`);
-      } else {
-        console.log(`Inserted data id:${this.lastID}`);
-      }
-      db.get(`SELECT * FROM user WHERE id = ?`, [this.lastID], (err, row) => {
+  db.run(
+    `CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)`,
+    () => {
+      console.log("Created books table.");
+      db.run(`INSERT INTO books (title) VALUES (?)`, function (err) {
         if (err) {
-          console.error(`Error selecting books: ${err.message}`);
+          console.error(`Error inserting books: ${err.message}`);
         } else {
-          console.log(row);
+          console.log(`Inserted data id:${this.lastID}`);
         }
-        db.run(`DROP TABLE books`, () => {
-          console.log(`DROP TABLE books`);
-          db.close(() => {
-            console.log("Closed the database connection.");
+        db.get(`SELECT * FROM user WHERE id = ?`, [this.lastID], (err, row) => {
+          if (err) {
+            console.error(`Error selecting books: ${err.message}`);
+          } else {
+            console.log(row);
+          }
+          db.run(`DROP TABLE books`, () => {
+            console.log(`DROP TABLE books`);
+            db.close(() => {
+              console.log("Closed the database connection.");
+            });
           });
         });
       });
-    });
-  },
-);
+    },
+  );
+});
