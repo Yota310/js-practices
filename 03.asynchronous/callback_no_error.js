06805aaaabc5f1eb64bc6236234ac15ca77f8610
yota sato <jingangsihaiuei@gmail.com>
2024-06-25 15:50:44 +0900

    インデントの修正

--- 03.asynchronous/callback_no_error.js
+++ 03.asynchronous/callback_no_error.js
@@ -9,19 +9,15 @@ const db = new sqlite3.Database(":memory:", () => {
       console.log("Created books table.");
       db.run("INSERT INTO books (title) VALUES (?)", ["桃太郎"], function () {
         console.log(`Inserted data id:${this.lastID}`);
-        db.get(
-          "SELECT * FROM books WHERE id = ?",
-          [this.lastID],
-          (_, row) => {
-            console.log(row);
-            db.run("DROP TABLE books", () => {
-              console.log("Dropped table books");
-              db.close(() => {
-                console.log("Closed the database connection.");
-              });
+        db.get("SELECT * FROM books WHERE id = ?", [this.lastID], (_, row) => {
+          console.log(row);
+          db.run("DROP TABLE books", () => {
+            console.log("Dropped table books");
+            db.close(() => {
+              console.log("Closed the database connection.");
             });
-          },
-        );
+          });
+        });
       });
     },
   );
