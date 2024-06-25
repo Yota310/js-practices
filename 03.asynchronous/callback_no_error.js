1472b8e4935a28a0aa3792f5a34533ea84342681
yota sato <jingangsihaiuei@gmail.com>
2024-06-25 15:32:36 +0900

    不要なテンプレート文字列を修正

--- 03.asynchronous/callback_no_error.js
+++ 03.asynchronous/callback_no_error.js
@@ -4,18 +4,18 @@ console.log("エラーなし");
 const db = new sqlite3.Database(":memory:", () => {
   console.log("Connected to the in-memory SQlite database.");
   db.run(
-    `CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT UNIQUE NOT NULL)`,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT UNIQUE NOT NULL)",
     () => {
       console.log("Created books table.");
-      db.run(`INSERT INTO books (title) VALUES (?)`, ["桃太郎"], function () {
+      db.run("INSERT INTO books (title) VALUES (?)", ["桃太郎"], function () {
         console.log(`Inserted data id:${this.lastID}`);
         db.get(
-          `SELECT * FROM books WHERE id = ?`,
+          "SELECT * FROM books WHERE id = ?",
           [this.lastID],
           (err, row) => {
           console.log(row);
-          db.run(`DROP TABLE books`, () => {
-            console.log(`DROP TABLE books`);
+          db.run("DROP TABLE books", () => {
+            console.log("DROP TABLE books");
             db.close(() => {
               console.log("Closed the database connection.");
             });
