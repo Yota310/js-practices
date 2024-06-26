f3b6820987ea1eca2a7805ef22a684e6e6eee758
yota sato <jingangsihaiuei@gmail.com>
2024-06-26 14:48:45 +0900

    promise_functionのコールバック関数の返り値の変更に伴った修正

--- 03.asynchronous/promise_error.js
+++ 03.asynchronous/promise_error.js
@@ -6,54 +6,42 @@ import {
 } from "./promise_function.js";
 
 console.log("エラーあり");
+
+let db;
+let lastID;
+
 openDatabase()
-  .then((db) => {
+  .then((responseDb) => {
+    db = responseDb;
     console.log("Connected to the in-memory SQlite database.");
-    return runQuery(
-      db,
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT UNIQUE NOT NULL)");
+    return runQuery(db,"CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)");
   })
-  .then((response) => {
+  .then(() => {
     console.log("Created books table.");
-    return response;
-  })
-  .then((response) => {
-    const db = response.db;
     return runQuery(db, "INSERT INTO books (title) VALUES (?)", [])
   })
   .then((response) => {
     if (response.err === undefined) {
-    const lastID = response.result.lastID;
+    lastID = response.lastID;
     console.log(`Inserted data id:${lastID}`);
     }
     return response
   })
   .catch((errResponse) => {
-    const db = errResponse.db
-    const err = errResponse.err
-    console.error(`Error inserting books: ${err.message}`);
-    
-    const response = {result: {lastID: 0}, db, err}
-    return response
+    console.error(`Error inserting books: ${errResponse.message}`);
+    return errResponse;
   })
   .then((response) => {
-    const db = response.db;
-    const lastID = response.result.lastID;
+    lastID = response.lastID;
     return getQuery(db, "SELECT * FROM user WHERE id = ?", lastID)
   })
   .catch((errResponse) => {
-    const db = errResponse.db
-    const err = errResponse.err
-    console.error(`Error selecting books: ${err.message}`);
-    const response = {result: {lastID: 0}, db}
-    return response
+    console.error(`Error selecting books: ${errResponse.message}`);
   })
-  .then((response) => {
-    const db = response.db;
+  .then(() => {
     return runQuery(db, "DROP TABLE books");
   })
-  .then((response) => {
-    const db = response.db;
+  .then(() => {
     console.log("DROP TABLE books");
     return closeDatabase(db);
   })
--- 03.asynchronous/promise_no_error.js
+++ 03.asynchronous/promise_no_error.js
@@ -6,35 +6,27 @@ import {
 } from "./promise_function.js";
 
 console.log("エラーなし");
+let db;
 openDatabase()
-  .then((db) => {
+  .then((responseDb) => {
+    db = responseDb;
     console.log("Connected to the in-memory SQlite database.");
-    return runQuery(
-      db,
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT UNIQUE NOT NULL)"
-    );
+    return runQuery(db,"CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE NOT NULL)");
   })
-  .then((response) => {
+  .then(() => {
     console.log("Created books table.");
-    return response;
-  })
-  .then((response) => {
-    const db = response.db;
     return runQuery(db, "INSERT INTO books (title) VALUES (?)", ["桃太郎"]);
   })
   .then((response) => {
-    const db = response.db;
-    const lastID = response.result.lastID;
+    const lastID = response.lastID;
     console.log(`Inserted data id:${lastID}`);
     return getQuery(db, "SELECT * FROM books WHERE id = ?", lastID);
   })
   .then((response) => {
-    console.log(response.row);
-    const db = response.db;
+    console.log(response);
     return runQuery(db, "DROP TABLE books");
   })
-  .then((response) => {
-    const db = response.db;
+  .then(() => {
     console.log("DROP TABLE books");
     return closeDatabase(db);
   })
