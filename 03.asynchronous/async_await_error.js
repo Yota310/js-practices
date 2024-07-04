1dd8e213673b8e3e1c3a65ded7323d6af417ab52
yota sato <jingangsihaiuei@gmail.com>
2024-07-04 17:50:58 +0900

    例外を補足できる様に修正

--- 03.asynchronous/async_await_error.js
+++ 03.asynchronous/async_await_error.js
@@ -7,8 +7,6 @@ import {
 
 console.log("エラーあり");
 
-
-
 const db = await openDatabase();
 console.log("Connected to the in-memory SQLite database.");
 
@@ -22,8 +20,11 @@ try {
   result = await runQuery(db, "INSERT INTO books (title) VALUES (?)");
   console.log(`Inserted data id:${result.lastID}`);
 } catch (err) {
-  console.error(`Error inserting books: ${err.message}`);
-  throw err;
+  if (err.errno === 19) {
+    console.error(`Error inserting books: ${err.message}`);
+  } else {
+    throw err;
+  }
 }
 try {
   const row = await getQuery(db, "SELECT * FROM user WHERE id = ?", [
@@ -31,8 +32,11 @@ try {
   ]);
   console.log("Selected data:", row);
 } catch (err) {
-  console.error(`Error selecting books: ${err.message}`);
-  throw err;
+  if (err.errno === 1) {
+    console.error(`Error selecting books: ${err.message}`);
+  } else {
+    throw err;
+  }
 }
 
 await runQuery(db, "DROP TABLE books");
